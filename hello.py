532458820ab118c6006f74a646895620c6742819
kavichen <chen1qi2wei3@gmail.com>
2013-09-27 14:47:32 +0800

    utf8

--- hello.py
+++ hello.py
@@ -1,13 +1,15 @@
+#coding=utf-8
 import werobot
 
 robot = werobot.WeRoBot(token='kavichen')
 
 @robot.handler
 def echo(message):
-    return 'Hello World!'
+  return 'Hello World!'
 
 @robot.subscribe
 def subscribe(message):
-	return 'guanzhu'
+  return '关注'
 
+@robot.
 robot.run()
