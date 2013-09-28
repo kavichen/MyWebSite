76a6825b28e7584c512598214cd6c1877d74112d
kavichen <chen1qi2wei3@gmail.com>
2013-09-28 10:15:04 +0800

    text reply test

--- hello.py
+++ hello.py
@@ -21,6 +21,16 @@ def echo(message):
   if message.content=='陈琦威'.decode('utf-8'):
       reply=TextReply(message = message, content = 'jianren')
       return reply
+  elif message.content == 'm':
+      reply=ArticlesReply(message=message)
+      article = Article(
+          title="mm",
+          description="test",
+          img="http://ww4.sinaimg.cn/large/5f581355gw1e91zrosf3sj20c10ezt9z.jpg",
+          url="http://chenqiwei.com"
+      )
+      reply.add_article(article)
+      return reply
   else:
       return message.content
 
