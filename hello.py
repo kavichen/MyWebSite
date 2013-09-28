34dcf61c212cf97866039c583f524dd76dca10f4
kavichen <chen1qi2wei3@gmail.com>
2013-09-28 10:26:53 +0800

    text reply test

--- hello.py
+++ hello.py
@@ -23,13 +23,19 @@ def echo(message):
       return reply
   elif message.content == 'x':
       reply=ArticlesReply(message=message)
-      article = Article(
+      article1 = Article(
           title="mm",
           description="test",
           img="http://ww4.sinaimg.cn/large/5f581355gw1e91zrosf3sj20c10ezt9z.jpg",
           url="http://ww4.sinaimg.cn/large/5f581355gw1e91zrosf3sj20c10ezt9z.jpg"
       )
-      reply.add_article(article)
+      article2 = Article(
+          title = "mm2",
+          description = "test2",
+          img="http://ww2.sinaimg.cn/large/5f581355gw1e9203prwxuj20d50jxt9u.jpg",
+          url="http://ww2.sinaimg.cn/large/5f581355gw1e9203prwxuj20d50jxt9u.jpg"
+      )
+      reply.add_article(article1,article2)
       return reply
   else:
       return message.content
